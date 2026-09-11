05fc386aa42dc387e1141e2deb08db186f06ca4a
Derek Milne <derek@milne.dev>
2026-09-11 14:40:48 -0700

    remove script

--- script.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById('contact-form');
-
-if (form) {
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    form.hidden = true;
-    document.getElementById('form-confirmation').hidden = false;
-  });
-}
